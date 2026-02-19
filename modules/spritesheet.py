4a0d896b38914f941bddd626a7bfcb861299ea86
qkuldo <teoa52269@gmail.com>
2026-02-19 18:46:34 +0800

    Added new attribute for `spritesheet` class to determine frame row
    Signed-off-by: qkuldo <teoa52269@gmail.com>

--- modules/spritesheet.py
+++ modules/spritesheet.py
@@ -5,9 +5,9 @@ class Spritesheet:
 		self.sheet = sheet
 		self.width = width
 		self.height = height
-	def load_frame(self,frame):
+	def load_frame(self,frame_column, frame_row=0):
 		# Load and return a specific frame from the spritesheet.
 		image = pg.Surface((self.width,self.height)).convert()
-		image.blit(self.sheet,(0,0),((frame*self.width),0,self.width,self.height))
+		image.blit(self.sheet,(0,0),((frame_column*self.width),(frame_row*self.width),self.width,self.height))
 		image.set_colorkey((0,0,0))
 		return image
