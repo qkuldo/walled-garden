c5568c412c5cb22fc216fc226c70ab90ec7bd999
qkuldo <teoa52269@gmail.com>
2026-03-02 11:28:50 +0800

    `levelGet()` can now display the proper room format

--- secret_level_editor.py
+++ secret_level_editor.py
@@ -1,1 +1,8 @@
 import game
+game.readAllJsonData()
+
+def levelGet(currentRoom):
+	roomLayout = list(game.ROOMTILEDATA[currentRoom].values())[3:18]
+	for tile in roomLayout:
+		print(tile)
+levelGet("test")
