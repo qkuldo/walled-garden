eb162e0d2c98cc60cb6bb87b0a3ee80652e9df6f
qkuldo <teoa52269@gmail.com>
2026-08-31 16:47:40 +0800

    Made random steer in both directions
    Signed-off-by: qkuldo <teoa52269@gmail.com>

--- game.py
+++ game.py
@@ -1087,18 +1087,20 @@ def game():
 				pg.draw.circle(DEBUGLAYER, BRIGHTYELLOW, (SCREENWIDTH/2,SCREENHEIGHT/2), 5)
 				for enemy in enemyList:
 					pg.draw.rect(DEBUGLAYER,ORANGE,enemy.hitbox)
-					extended_endpoint = modules.helper.goto_angle(3000, enemy.customAttributes["target angle"])
-					pg.draw.line(DEBUGLAYER,GREEN,enemy.hitbox.center,(enemy.hitbox.center[0]-extended_endpoint[0], enemy.hitbox.center[1]-extended_endpoint[1]), 2)
-					offsetPositive_endpoint = modules.helper.goto_angle(3000, enemy.customAttributes["target angle"]+enemy.customAttributes["looking offset"])
-					offsetNegative_endpoint = modules.helper.goto_angle(3000, enemy.customAttributes["target angle"]-enemy.customAttributes["looking offset"])
-					actual_offsetNegative_endpoint = modules.helper.goto_angle(3000, enemy.customAttributes["debug"][0])
-					actual_offsetPositive_endpoint = modules.helper.goto_angle(3000, enemy.customAttributes["debug"][1])
-					lookSteer_endpoint = modules.helper.goto_angle(3000, modules.helper.face_target(enemy.hitbox.center, Player.hitbox.center)+enemy.customAttributes["random steer"])
-					pg.draw.line(DEBUGLAYER,PALEBLUE,enemy.hitbox.center,(enemy.hitbox.center[0]-offsetPositive_endpoint[0], enemy.hitbox.center[1]-offsetPositive_endpoint[1]), 2)
-					pg.draw.line(DEBUGLAYER,PALEBLUE,enemy.hitbox.center,(enemy.hitbox.center[0]-offsetNegative_endpoint[0], enemy.hitbox.center[1]-offsetNegative_endpoint[1]), 2)
-					pg.draw.line(DEBUGLAYER,BLUE,enemy.hitbox.center,(enemy.hitbox.center[0]-actual_offsetNegative_endpoint[0], enemy.hitbox.center[1]-actual_offsetNegative_endpoint[1]), 2)
-					pg.draw.line(DEBUGLAYER,BLUE,enemy.hitbox.center,(enemy.hitbox.center[0]-actual_offsetPositive_endpoint[0], enemy.hitbox.center[1]-actual_offsetPositive_endpoint[1]), 2)
-					pg.draw.line(DEBUGLAYER,PALEGREEN,enemy.hitbox.center,(enemy.hitbox.center[0]-lookSteer_endpoint[0], enemy.hitbox.center[1]-lookSteer_endpoint[1]), 2)
+					if (debugSecMode == 0):
+						extended_endpoint = modules.helper.goto_angle(3000, enemy.customAttributes["target angle"])
+						pg.draw.line(DEBUGLAYER,GREEN,enemy.hitbox.center,(enemy.hitbox.center[0]-extended_endpoint[0], enemy.hitbox.center[1]-extended_endpoint[1]), 2)
+						offsetPositive_endpoint = modules.helper.goto_angle(3000, enemy.customAttributes["target angle"]+enemy.customAttributes["looking offset"])
+						offsetNegative_endpoint = modules.helper.goto_angle(3000, enemy.customAttributes["target angle"]-enemy.customAttributes["looking offset"])
+						actual_offsetNegative_endpoint = modules.helper.goto_angle(3000, enemy.customAttributes["debug"][0])
+						actual_offsetPositive_endpoint = modules.helper.goto_angle(3000, enemy.customAttributes["debug"][1])
+						pg.draw.line(DEBUGLAYER,PALEBLUE,enemy.hitbox.center,(enemy.hitbox.center[0]-offsetPositive_endpoint[0], enemy.hitbox.center[1]-offsetPositive_endpoint[1]), 2)
+						pg.draw.line(DEBUGLAYER,PALEBLUE,enemy.hitbox.center,(enemy.hitbox.center[0]-offsetNegative_endpoint[0], enemy.hitbox.center[1]-offsetNegative_endpoint[1]), 2)
+						pg.draw.line(DEBUGLAYER,BLUE,enemy.hitbox.center,(enemy.hitbox.center[0]-actual_offsetNegative_endpoint[0], enemy.hitbox.center[1]-actual_offsetNegative_endpoint[1]), 2)
+						pg.draw.line(DEBUGLAYER,BLUE,enemy.hitbox.center,(enemy.hitbox.center[0]-actual_offsetPositive_endpoint[0], enemy.hitbox.center[1]-actual_offsetPositive_endpoint[1]), 2)
+					elif (debugSecMode == 1):
+						lookSteer_endpoint = modules.helper.goto_angle(3000, modules.helper.face_target(enemy.hitbox.center, (Player.hitbox.center[0]+enemy.customAttributes["random steer"][0],Player.hitbox.center[1]+enemy.customAttributes["random steer"][1])))
+						pg.draw.line(DEBUGLAYER,PALEGREEN,enemy.hitbox.center,(enemy.hitbox.center[0]-lookSteer_endpoint[0], enemy.hitbox.center[1]-lookSteer_endpoint[1]), 2)
 				for dataIndex in range(0, len(distanceList)):
 					data = distanceList[dataIndex]
 					distance = distances[dataIndex]
--- modules/helper.py
+++ modules/helper.py
@@ -98,7 +98,7 @@ def moveEnemy(enemy, data, currentRoomData, Player, currentTime, isHit=False, sl
 	movement_vector = (0, 0)
 	checkCollisionList = copy.deepcopy(currentRoomData["collisionBoxes"])
 	checkCollisionList.append(Player.hitbox)
-	distance_fromPlayer = measureDistance(Player.hitbox.center, enemy.hitbox.center)
+	distance_fromOffsetPoint = measureDistance((Player.hitbox.center[0]+enemy.customAttributes["random steer"][0],Player.hitbox.center[1]+enemy.customAttributes["random steer"][1]), enemy.hitbox.center)
 	if (not slowdown):
 		durationMultiplier = 1
 	else:
@@ -108,9 +108,9 @@ def moveEnemy(enemy, data, currentRoomData, Player, currentTime, isHit=False, sl
 		if (enemy.customAttributes["strong attack"] == True):
 			longHitstun = 2
 		if (enemy.customAttributes["state"] == 1):
-			if (distance_fromPlayer < 500 and distance_fromPlayer > enemy.customAttributes["pursue distance"]):
+			if (distance_fromOffsetPoint < 500 and distance_fromOffsetPoint > enemy.customAttributes["pursue distance"]):
 				enemy.customAttributes["state"] = PURSUING
-			elif (distance_fromPlayer <= enemy.customAttributes["pursue distance"]):
+			elif (distance_fromOffsetPoint <= enemy.customAttributes["pursue distance"]):
 				enemy.customAttributes["state"] = WINDUP
 				enemy.customAttributes["state timer start"] = currentTime
 				enemy.customAttributes["target angle"] = face_target(enemy.hitbox.center, Player.hitbox.center)
@@ -122,7 +122,7 @@ def moveEnemy(enemy, data, currentRoomData, Player, currentTime, isHit=False, sl
 			if (timerFinishCheck(currentTime, enemy.customAttributes["state timer start"], enemy.customAttributes["attack duration"]*durationMultiplier)):
 				enemy.customAttributes["state timer start"] = currentTime
 				enemy.customAttributes["state"] = RECOVERY
-				enemy.customAttributes["random steer"] = random.randint(-enemy.customAttributes["steer offset"],enemy.customAttributes["steer offset"])
+				enemy.customAttributes["random steer"] = (random.randint(-enemy.customAttributes["steer offset"],enemy.customAttributes["steer offset"]),random.randint(-enemy.customAttributes["steer offset"],enemy.customAttributes["steer offset"]))
 		if (enemy.customAttributes["state"] == RECOVERY):
 			if (timerFinishCheck(currentTime, enemy.customAttributes["state timer start"], enemy.customAttributes["recovery duration"]*durationMultiplier)):
 				enemy.customAttributes["state timer start"] = currentTime
@@ -131,7 +131,7 @@ def moveEnemy(enemy, data, currentRoomData, Player, currentTime, isHit=False, sl
 			if (timerFinishCheck(currentTime, enemy.customAttributes["state timer start"], enemy.customAttributes["hitstun duration"]*durationMultiplier)):
 				enemy.customAttributes["state timer start"] = currentTime
 				enemy.customAttributes["strong attack"] = False
-				enemy.customAttributes["random steer"] = random.randint(-enemy.customAttributes["steer offset"],enemy.customAttributes["steer offset"])
+				enemy.customAttributes["random steer"] = (random.randint(-enemy.customAttributes["steer offset"],enemy.customAttributes["steer offset"]),random.randint(-enemy.customAttributes["steer offset"],enemy.customAttributes["steer offset"]))
 				if (distance_fromPlayer <= enemy.customAttributes["pursue distance"]):
 					enemy.customAttributes["state"] = WINDUP
 				else:
@@ -139,7 +139,7 @@ def moveEnemy(enemy, data, currentRoomData, Player, currentTime, isHit=False, sl
 		if (enemy.customAttributes["state"] == IDLE):
 			enemy.customAttributes["debug"] = basicIdle(enemy, durationMultiplier, currentTime, Player, deltaTime)
 		if (enemy.customAttributes["state"] == PURSUING):
-			movement_vector = goto_angleComplex(enemy, speed_multiplier=1, angle=face_target(enemy.hitbox.center, Player.hitbox.center)+enemy.customAttributes["random steer"], targetPos=Player.hitbox.center, checkCollision=True, collisionList=checkCollisionList, setDir = True) 
+			movement_vector = goto_angleComplex(enemy, speed_multiplier=1, angle=face_target(enemy.hitbox.center, (Player.hitbox.center[0]+enemy.customAttributes["random steer"][0],Player.hitbox.center[1]+enemy.customAttributes["random steer"][1])), targetPos=Player.hitbox.center, checkCollision=True, collisionList=checkCollisionList, setDir = True) 
 		elif (enemy.customAttributes["state"] == ATTACK):
 			movement_vector = goto_angleComplex(enemy, speed_multiplier=2, angle=enemy.customAttributes["target angle"], targetPos=Player.hitbox.center, checkCollision=True, collisionList=currentRoomData["collisionBoxes"], setDir = True)
 		if (not slowdown):
@@ -174,7 +174,7 @@ def basicIdle(enemy,durationMultiplier, currentTime, Player, deltaTime):
 		enemy.customAttributes["look timer start"] = copy.deepcopy(currentTime)
 		if (enemy.customAttributes["player see timer"] >= enemy.customAttributes["aggression time"]):
 			enemy.customAttributes["state"] = PURSUING
-			enemy.customAttributes["random steer"] = random.randint(-enemy.customAttributes["steer offset"],enemy.customAttributes["steer offset"])
+			enemy.customAttributes["random steer"] = (random.randint(-enemy.customAttributes["steer offset"],enemy.customAttributes["steer offset"]),random.randint(-enemy.customAttributes["steer offset"],enemy.customAttributes["steer offset"]))
 	else:
 		enemy.customAttributes["player see timer"] = 0
 	return betweenAngles(targetAngleOffsets[0], targetAngleOffsets[1], playerFindAngle, True)
